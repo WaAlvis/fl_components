7deaeb1870b4212af85953cb7915b408c3a216ab
William Andres Alvis Palencia <alvis.wap@gmail.com>
2023-10-04 08:48:11 -0500

    agregando Cards components

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
@@ -8,6 +9,41 @@ class CardScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Card Screen'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        children: [
+          Card(
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Icon(
+                    Icons.public,
+                  ),
+                  title: Text('Este es un titulo'),
+                  subtitle: Text(
+                      'Q. Ea pariatur nostrud nulla consequat mollit incididunt occaecat proident anim proident excepteur laborum sunt irure. Aliquip cillum esse do esse aliquip. Non cupidatat dolore duis elit amet esse non.'),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('Cancel'),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('Ok'),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
