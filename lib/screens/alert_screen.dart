aeaddbb0f9a94f066600690c02c7d31cc4c6e9b6
William Andres Alvis Palencia <alvis.wap@gmail.com>
2023-10-05 16:38:16 -0500

    agregando Alert Dialogs de ambas plataformas iOS y Android

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,13 +1,99 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({super.key});
 
+  void displayDialogAndroid(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Este es Un Alert Android'),
+          content: const Column(mainAxisSize: MainAxisSize.min, children: [
+            Text('Este es el conteido del AlertDialog'),
+            SizedBox(
+              height: 20,
+            ),
+            FlutterLogo(
+              size: 100,
+            )
+          ]),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cerrar'),
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  void displayDialogIOS(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: const Text('Este es Un Alert IOS'),
+          content: const Column(mainAxisSize: MainAxisSize.min, children: [
+            Text('Este es el conteido del AlertDialog'),
+            SizedBox(
+              height: 20,
+            ),
+            FlutterLogo(
+              size: 100,
+            )
+          ]),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cerrar'),
+            )
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Alert Screen'),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.close),
+        onPressed: () => Navigator.pop(context),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton.icon(
+              label: const Text('Mostar Alerta'),
+              icon: const Icon(Icons.android),
+              onPressed: () => displayDialogAndroid(context),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'iOS',
+                    style: TextStyle(fontSize: 17),
+                  ),
+                  Text(' Mostar Alerta'),
+                ],
+              ),
+              onPressed: () => displayDialogIOS(context),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const primaryColor = Colors.amber;
+  static const primaryColor = Colors.indigo;
 
   static final ThemeData themeAppLight = ThemeData.light().copyWith(
     primaryColor: primaryColor,
@@ -10,6 +10,19 @@ class AppTheme {
         foregroundColor: primaryColor,
       ),
     ),
+
+    floatingActionButtonTheme:
+        const FloatingActionButtonThemeData(backgroundColor: primaryColor),
+ 
+        
+
+
+    elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 20),
+            backgroundColor: primaryColor,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20)))),
     appBarTheme: const AppBarTheme(elevation: 0, backgroundColor: primaryColor),
   );
 
